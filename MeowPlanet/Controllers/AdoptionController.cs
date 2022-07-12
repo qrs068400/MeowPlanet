8bffcb8589124a7ff816ad6ad04e41b91624ea95
CR1-23\Student <ckiwee@gmail.com>
2022-07-12 16:31:59 +0800

    領養

---
+++ MeowPlanet/Controllers/AdoptionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeowPlanet.Controllers
+{
+    public class AdoptionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
