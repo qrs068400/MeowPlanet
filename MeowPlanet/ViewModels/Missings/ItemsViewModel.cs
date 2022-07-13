722f086604b3e50f4026ecbc881732bded86247f
DESKTOP-69CPJ46\soga5 <soga5731@gmail.com>
2022-07-13 23:32:16 +0800

    add viewmodels

---
+++ MeowPlanet/ViewModels/Missings/ItemsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MeowPlanet.ViewModels.Missings
+{
+    public class ItemsViewModel
+    {
+        public int MissingId { get; set; }
+        public string? Image { get; set; }
+        public string? Name { get; set; }
+        public string? Breed { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
