f0201dd001a7688a830bfd3d2240cf3e35117c63
CR1-10\Student <qrs068400@gmail.com>
2022-07-27 15:45:09 +0800

    no message

--- MeowPlanet/Models/Sitter.cs
+++ MeowPlanet/Models/Sitter.cs
@@ -16,21 +16,19 @@ namespace MeowPlanet.Models
         public int MemberId { get; set; }
         public string Name { get; set; } = null!;
         public string Summary { get; set; } = null!;
+        public string Sleep { get; set; } = null!;
+        public string House { get; set; } = null!;
+        public string Outdoor { get; set; } = null!;
         public int Pay { get; set; }
-        public string Licence { get; set; } = null!;
-        public string Cage { get; set; } = null!;
-        public string Monitor { get; set; } = null!;
-        public string Meal { get; set; } = null!;
-        public string CatNumber { get; set; } = null!;
         public decimal PosLat { get; set; }
         public decimal PosLng { get; set; }
         public bool IsService { get; set; }
-        public decimal AvgStar { get; set; }
         public string? Img01 { get; set; }
         public string? Img02 { get; set; }
         public string? Img03 { get; set; }
         public string? Img04 { get; set; }
         public string? Img05 { get; set; }
+        public decimal? AvgStar { get; set; }
 
         public virtual Member Member { get; set; } = null!;
         public virtual ICollection<Favorite> Favorites { get; set; }
