0038445c90a4906846129d59382ef51c0d56eb76
Ren <a1278001@gmail.com>
2022-07-27 15:55:33 +0800

    no message

--- MeowPlanet/Controllers/MissingsController.cs
+++ MeowPlanet/Controllers/MissingsController.cs
@@ -6,29 +6,48 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MeowPlanet.ViewModels.Missings;
-
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.Data.SqlClient;
 
 namespace MeowPlanet.Models
 {
     public class MissingsController : Controller
     {
         private readonly endtermContext _context;
+        private readonly int? _memberId;
 
-        public MissingsController(endtermContext context)
+        public MissingsController(endtermContext context, IHttpContextAccessor contextAccessor)
         {
             _context = context;
+
+            if (contextAccessor.HttpContext.User.FindFirst(ClaimTypes.Sid) != null)
+            {
+                _memberId = Convert.ToInt32(contextAccessor.HttpContext.User.FindFirst(ClaimTypes.Sid).Value);
+            }
+            
         }
 
-        // GET: Missings
-        public ActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var itemList = _context.ItemsViewModels.FromSqlRaw("SELECT missing.missing_id AS MissingId, sex AS Sex, img_01 AS Image, cat.name AS Name, date AS MissingDate, cat_breed.name AS Breed, COUNT(clue.clue_id) AS ClueCount, MAX(witness_time) AS UpdateDate FROM missing INNER JOIN cat ON cat.cat_id = missing.cat_id INNER JOIN cat_breed ON cat.breed_id = cat_breed.breed_id LEFT JOIN clue ON missing.missing_id = clue.missing_id WHERE is_found = 0 GROUP BY missing.missing_id, sex, img_01, cat.name, date, cat_breed.name").ToList();
+            List<ItemsViewModel> itemList;
+
+            if (_memberId != null)
+            {
+                var memberId = new SqlParameter("memberId", _memberId);
+                itemList = await _context.ItemsViewModels.FromSqlRaw("SELECT missing.missing_id AS MissingId, sex AS Sex, img_01 AS Image, cat.name AS Name, date AS MissingDate, cat_breed.name AS Breed, COUNT(clue.clue_id) AS ClueCount, MAX(witness_time) AS UpdateDate FROM missing INNER JOIN cat ON cat.cat_id = missing.cat_id INNER JOIN cat_breed ON cat.breed_id = cat_breed.breed_id LEFT JOIN clue ON missing.missing_id = clue.missing_id WHERE is_found = 0 and cat.member_id != @memberId GROUP BY missing.missing_id, sex, img_01, cat.name, date, cat_breed.name", memberId).ToListAsync();
+            }
+            else
+            {
+                itemList = await _context.ItemsViewModels.FromSqlRaw("SELECT missing.missing_id AS MissingId, sex AS Sex, img_01 AS Image, cat.name AS Name, date AS MissingDate, cat_breed.name AS Breed, COUNT(clue.clue_id) AS ClueCount, MAX(witness_time) AS UpdateDate FROM missing INNER JOIN cat ON cat.cat_id = missing.cat_id INNER JOIN cat_breed ON cat.breed_id = cat_breed.breed_id LEFT JOIN clue ON missing.missing_id = clue.missing_id WHERE is_found = 0 GROUP BY missing.missing_id, sex, img_01, cat.name, date, cat_breed.name").ToListAsync();
+            }
+                        
 
             return View(itemList);
         }
 
 
-        [HttpPost]
+        [HttpPost] //未完成
         public async Task<IActionResult> AddMissing(Missing missingCat)
         {
             _context.Missings.Add(missingCat);
@@ -40,44 +59,36 @@ namespace MeowPlanet.Models
 
 
         [HttpGet]
-        public ActionResult GetMissing()
+        public async Task<IActionResult> GetMissing()
         {
-            List<Missing> catList = new List<Missing>();
-            foreach (var item in _context.Missings)
-            {
-                Missing missingCat = new Missing
-                {
-                    MissingId = item.MissingId,
-                    CatId = item.CatId,
-                    Date = item.Date,
-                    Lat = item.Lat,
-                    Lng = item.Lng
-                };
+            List<Missing> catList;
 
-                catList.Add(missingCat);
+            if (_memberId != null)
+            {
+                catList = await _context.Missings.Include(x => x.Cat)
+                    .Where(x => x.Cat.MemberId != _memberId && x.IsFound == false).ToListAsync();
+            }
+            else
+            {
+                catList = await _context.Missings.ToListAsync();
             }
 
             return Json(catList);
         }
 
-        //public ActionResult GetItems()
-        //{
-        //    var itemList = _context.ItemsViewModels.FromSqlRaw("SELECT missing.missing_id AS MissingId, sex AS Sex, img_01 AS Image, cat.name AS Name, date AS MissingDate, cat_breed.name AS Breed, COUNT(clue.clue_id) AS ClueCount, MAX(witness_time) AS UpdateDate FROM missing INNER JOIN cat ON cat.cat_id = missing.cat_id INNER JOIN cat_breed ON cat.breed_id = cat_breed.breed_id LEFT JOIN clue ON missing.missing_id = clue.missing_id WHERE is_found = 0 GROUP BY missing.missing_id, sex, img_01, cat.name, date, cat_breed.name").ToList();
-
-        //    return PartialView("_MissingItemsPartial", itemList);
-        //}
 
 
-        public ActionResult GetDetail(int missingId)
+
+        public async Task<IActionResult> GetDetail(int missingId)
         {
-            var result = _context.Missings
+            var result = await _context.Missings
                 .Where(x => x.MissingId == missingId)
                 .Include(x => x.Cat)
                 .Include(x => x.Cat.Member)
                 .Include(x => x.Cat.Breed)
                 .Select(x => new DetailViewModel()
-                {                    
-                    Name = x.Cat.Name,                    
+                {
+                    Name = x.Cat.Name,
                     Sex = x.Cat.Sex,
                     Age = x.Cat.Age,
                     Breed = x.Cat.Breed.Name,
@@ -89,14 +100,53 @@ namespace MeowPlanet.Models
                     MemberName = x.Cat.Member.Name,
                     Photo = x.Cat.Member.Photo
                 })
-                .FirstOrDefault();
+                .FirstOrDefaultAsync();
 
             return PartialView("_MissingDetailPartial" ,result);
         }
 
-        public ActionResult GetPublish()
-        {
+        public ActionResult GetPublish()  //未完成
+        {            
             return PartialView("_MissingPublishPartial");
+        }
+
+        public string PostClue(decimal WitnessLat, decimal WitnessLng, IFormFile Image, DateTime WitnessTime, string Description, int MissingId, [FromServices] IWebHostEnvironment _webHostEnvironment)
+        {
+            var clue = new Clue
+            {
+                MissingId = MissingId,
+                WitnessLat = WitnessLat,
+                WitnessLng = WitnessLng,
+                WitnessTime = WitnessTime,
+                Description = Description,
+                MemberId = (int)_memberId
+            };
+
+            Random random = new Random();
+            string? uniqueFileName;
+
+            if (Image != null)
+            {
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "Images/clue");
+                uniqueFileName = DateTime.Now.ToString("yyyyMMddHHmmss") + random.Next(1000, 9999).ToString() + Image.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    Image.CopyTo(fileStream);
+                }
+                clue.ImagePath = "/images/clue/" + uniqueFileName;
+            }
+
+            _context.Clues.Add(clue);
+            _context.SaveChanges();
+
+            return "OK";
+        }
+
+        [HttpGet]
+        public IActionResult GetClues()
+        {
+            return PartialView("_MissingCluePartial");
         }
     }
 }
--- MeowPlanet/Models/Adopt.cs
+++ MeowPlanet/Models/Adopt.cs
@@ -5,11 +5,13 @@ namespace MeowPlanet.Models
 {
     public partial class Adopt
     {
+
         public int MemberId { get; set; }
         public int CatId { get; set; }
+        public int Owner { get; set; }
         public DateTime DateStart { get; set; }
         public DateTime? DateOver { get; set; }
-        public bool Status { get; set; }
+        public int Status { get; set; }
 
         public virtual Cat Cat { get; set; } = null!;
         public virtual Member Member { get; set; } = null!;
