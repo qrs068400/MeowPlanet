4be394c10cc436dbe5920914e24732d1bdef5be4
DESKTOP-69CPJ46\soga5 <soga5731@gmail.com>
2022-07-29 20:58:24 +0800

    debug

--- MeowPlanet/ViewComponents/MissingsManageViewComponent.cs
+++ MeowPlanet/ViewComponents/MissingsManageViewComponent.cs
@@ -23,7 +23,7 @@ namespace MeowPlanet.ViewComponents
             var result = _context.Clues.Include(c => c.Missing)
                 .Include(c => c.Missing.Cat)
                 .Where(c => c.Missing.Cat.MemberId == memberId && c.Missing.Cat.IsMissing == true)
-                .Where(c => c.Missing.CatId == c.Missing.Cat.CatId)
+                .Where(c => c.Missing.CatId == c.Missing.Cat.CatId && c.Missing.IsFound == false)
                 .Where(c => c.Missing.MissingId == c.MissingId && c.Status == status)
                 .Select(c => new ClueViewModel
                 {
