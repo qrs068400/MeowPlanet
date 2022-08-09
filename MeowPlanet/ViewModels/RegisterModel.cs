068531df213841f642a41dd6f5eeb124f7ad1aff
CR1-10\Student <qrs068400@gmail.com>
2022-08-09 15:25:52 +0800

    no message

--- MeowPlanet/ViewModels/RegisterModel.cs
+++ MeowPlanet/ViewModels/RegisterModel.cs
@@ -10,10 +10,11 @@ namespace MeowPlanet.ViewModels
         [EmailAddress(ErrorMessage = "請輸入有效的Email")]
         public new string Email { get; set; } = null!;
         [Required(ErrorMessage = "請輸入有效的密碼")]
+        [StringLength(10, MinimumLength = 6, ErrorMessage = "密碼必須是6-10位")]
         public new string Password { get; set; } = null!;
         [Required(ErrorMessage = "請輸入有效的手機號碼")]
         [RegularExpression(@"^09[0-9]{8}$",ErrorMessage =  "手機號碼格式不正確")]
-        public string Phone { get; set; } = null!;
+        public new string Phone { get; set; } = null!;
         [Required(ErrorMessage = "請輸入有效的名字")]
         public new string Name { get; set; } = null!;
         public new string? Photo { get; set; }
