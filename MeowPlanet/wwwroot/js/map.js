df413404b759580feab1ee620f16f48221e95070
DESKTOP-69CPJ46\soga5 <soga5731@gmail.com>
2022-08-10 16:57:39 +0800

    Fix searching zooming

--- MeowPlanet/wwwroot/js/map.js
+++ MeowPlanet/wwwroot/js/map.js
@@ -39,7 +39,7 @@ function initMap() {
         center: { lat: 22.629314218928563, lng: 120.29299528465663 },
         zoom: 16,
         minZoom: 14,
-        maxZoom: 17,
+        maxZoom: 18,
         disableDefaultUI: true,
         mapId: 'a5f4cec6781c8dda',
         gestureHandling: 'greedy',
@@ -125,6 +125,7 @@ function initMap() {
 
     map.addListener('zoom_changed', () => {
         let zoom = map.getZoom();
+        console.log(zoom);
         switch (zoom) {
             case 14:
                 scaledRange = 4000;
@@ -138,13 +139,16 @@ function initMap() {
             case 17:
                 scaledRange = 500;
                 break;
-            default:
+            case 18:
+                scaledRange = 250;
+                break;
+            default:                
                 Swal.fire({
                     heightAuto: false,
                     position: 'center',
                     icon: 'warning',
                     title: '搜尋的地區超出縮放範圍',
-                    text: '請執行重新搜尋',
+                    text: '請重新執行搜尋',
                     showConfirmButton: false,
                     timer: 2000
                 })
